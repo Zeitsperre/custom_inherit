9ce59d3e79898379745dc8cc065097ca59e26460
Zeitsperre <trevor_smith@live.com>
2019-10-11 16:59:49 -0400

    Blacken

--- src/custom_inherit/_style_store.py
+++ src/custom_inherit/_style_store.py
@@ -1,7 +1,11 @@
 from __future__ import absolute_import
 
-from ._doc_parse_tools import (merge_google_napoleon_docs, merge_numpy_docs,
-                               merge_numpy_napoleon_docs, merge_rest_docs)
+from ._doc_parse_tools import (
+    merge_google_napoleon_docs,
+    merge_numpy_docs,
+    merge_numpy_napoleon_docs,
+    merge_rest_docs,
+)
 
 """ Docstring inheritance-style implementations.
 
